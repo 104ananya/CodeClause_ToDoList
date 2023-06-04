150db0bb7af7792340304f9c74fd7e65dadbe929
KUMARI ANANYA <75521183+104ananya@users.noreply.github.com>
2023-06-04 16:42:51 +0530

    javascript

--- script.js
+++ script.js
@@ -1,6 +1,9 @@
 const taskInput = document.querySelector(".task-input input");
 taskBox = document.querySelector(".task-box");
 
+let editId;
+let isEdited = false;
+
 // GET LOCALSTORAGE TODO-LIST
 let todos = JSON.parse(localStorage.getItem("todo-list"));
 
@@ -12,15 +15,19 @@ function showTodo() {
     todos.forEach((todo, id) => {
       // console.log(id, todo);
 
+      // ADDING THE CLASS 'CHECKED' IF IT HAS BEEN COMPLETED or TO PREVENT IT FROM GETTING 'UNCHECKED' DUE TO REFRESHING THE PAGE
+      let isCompleted = todo.status == "Completed" ? "checked" : "";
+      // if completed then set isCompleted value to checked
+
       list += `<li class="task">
-                    <label for="${id}">
+                    <label for="">
                         <div class="content">
-                            <input onclick="updateStatus(this)" type="checkbox" id="${id}">
-                            <p>${todo.name}</p>
+                            <input onclick="updateStatus(this)" type="checkbox" id="${id}" ${isCompleted}>
+                            <p class="${isCompleted}">${todo.name}</p>
                         </div>
                         <div class="setting">
-                            <i class="uil uil-edit-alt"></i>
-                            <i class="uil uil-trash-alt"></i>
+                            <span onclick="editTask(${id}, '${todo.name}')" ><i class="uil uil-edit-alt"></i></span>
+                            <span onclick="deleteTask(${id})" ><i class="uil uil-trash-alt"></i></span>
                         </div>
 
                     </label>
@@ -33,24 +40,50 @@ function showTodo() {
 
 showTodo();
 
-function updateStatus(selectedTask){
+// FUNCTION TO UPDATE STATUS
+function updateStatus(selectedTask) {
+  // GETTING PARAGRAPH THAT CONTAINS CLASS NAME
+  let taskName = selectedTask.parentElement.lastElementChild;
 
+  // TO ADD or REMOVE CHECKED CLASS DYNAMICALLY
+  if (selectedTask.checked) {
+    taskName.classList.add("checked");
+    todos[selectedTask.id].status = "Completed";
+  } else {
+    taskName.classList.remove("checked");
+    todos[selectedTask.id].status = "Pending";
+  }
 
-    // GETTING PARAGRAPH THAT CONTAINS CLASS NAME
-    let taskName = selectedTask.parentElement.lastElementChild;
+  // MODIFING STATUS ON LOCAL STORAGE AS WELL
+  localStorage.setItem("todo-list", JSON.stringify(todos));
 
-    // TO ADD CHECKED CLASS DYNAMICALLY
-    if(selectedTask.checked){
-        taskName.classList.add("checked")
-    }
-    else{
-        taskName.classList.remove("checked")
-    }
-
-    // console.log(selectedTask);
+  // console.log(selectedTask);
 }
 
+// FUNCTION TO DELETE TASK
 
+function deleteTask(deleteId) {
+  console.log(deleteId);
+
+  // REMOVE SELECTED TASK FROM ARRAY OR TODOS
+  todos.splice(deleteId, 1);
+
+  // UPDATE THE TODOS ARRAY AGAIN INTO THE LOCALHOST
+  localStorage.setItem("todo-list", JSON.stringify(todos));
+
+  // SHOW THE UPDATED TASK BOX AGAIN
+  showTodo();
+}
+
+// FUNCTION TO EDIT TASK
+
+function editTask(taskId, taskName) {
+  // console.log(taskId);
+
+  editId = taskId;
+  isEdited = true;
+  taskInput.value = taskName;
+}
 
 taskInput.addEventListener("keyup", (e) => {
   let userTask = taskInput.value.trim();
@@ -58,20 +91,28 @@ taskInput.addEventListener("keyup", (e) => {
   if (e.key == "Enter" && userTask) {
     // console.log(userTask)
 
-    if (!todos) {
-      todos = [];
-      
-      // IF TODOS IS EMPTY THEN PASS EMPTY ARRAY TO TODOS
+    // IF isEdited IS FALSE
+    if (!isEdited) {
+      if (!todos) {
+        todos = [];
+        // IF TODOS IS EMPTY THEN PASS EMPTY ARRAY TO TODOS
+      }
+
+      let taskInfo = { name: userTask, status: "pending" };
+
+      // ADD NEW TASK
+      todos.push(taskInfo);
     }
 
-    
+    // IF isEdited TRUE
+    else {
+      isEdited = false;
+      todos[editId].name = userTask;
+    }
+
     // SETTING TASK INPUT VALUE TO EMPTY AGAIN
     taskInput.value = "";
 
-    let taskInfo = { name: userTask, status: "pending" };
-
-    // ADD NEW TASK
-    todos.push(taskInfo);
     localStorage.setItem("todo-list", JSON.stringify(todos));
 
     // MAKE IT APPEAR ON TASKBOX
